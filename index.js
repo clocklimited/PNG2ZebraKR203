db0cbddb8a4e249896eb7a1a3613537e67d28335
Will Stewart <willstewart.decider@gmail.com>
2018-10-05 10:02:52 +0100

    Export printer function

--- index.js
+++ index.js
@@ -7,27 +7,21 @@ const {
   secondaryBurnSpeed
 } = require('./lib/util')
 
-const args = require('args')
 const pngToMonoLiner = require('./lib/png-mono-liner')
-const fs = require('fs')
 const PNG = require('pngjs').PNG
 
-args.option('input', 'PNG filename. STDIN will be used if omitted.')
+export default fileData => {
+  const png = PNG.sync.read(fileData)
 
-const flags = args.parse(process.argv)
-const filename = flags.input || 0
-
-const data = fs.readFileSync(filename)
-const png = PNG.sync.read(data)
-
-const job = Buffer.from([
-  ...primaryBurnSpeed(500),
-  ...secondaryBurnSpeed(120),
-  ...setup,
-  ...pngToMonoLiner(png).reduce(
-    (collection, line) => collection.concat(...lineOutput(line)),
-    []
-  ),
-  ...feedAndCut
-])
-process.stdout.write(job)
+  const job = Buffer.from([
+    ...primaryBurnSpeed(500),
+    ...secondaryBurnSpeed(120),
+    ...setup,
+    ...pngToMonoLiner(png).reduce(
+      (collection, line) => collection.concat(...lineOutput(line)),
+      []
+    ),
+    ...feedAndCut
+  ])
+  process.stdout.write(job)
+}
