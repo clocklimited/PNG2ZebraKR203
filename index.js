9c3715f00e939f78a13308d43924e48b9e4c29f3
Paul Serby <paul.serby@clock.co.uk>
2018-10-12 15:08:59 +0100

    Add timings

--- index.js
+++ index.js
@@ -15,27 +15,32 @@ const pngToMonoLiner = require('./lib/png-mono-liner')
 const PNG = require('pngjs').PNG
 
 module.exports = function(fileData) {
+  
   console.log('Printer reached \n\n\n\n\n\n***********************\n\n\n\n\n\n')
-  console.log(4634, fileData)
+  const startTime = Date.now() 
   const png = PNG.sync.read(fileData)
-
+  console.log('PNG Read', Date.now() - startTime)
+  
   const job = Buffer.from([
     ...primaryBurnSpeed(500),
     ...secondaryBurnSpeed(120),
     ...setup,
     ...feed
   ])
-  console.log(93523, job)
+  console.log('Buffer Created', Date.now() - startTime)
   const printer = fs.createWriteStream('/dev/usb/lp0')
+  console.log('Connected to printer', Date.now() - startTime)
   printer.write(job)
-
+  console.log('Write Job Header', Date.now() - startTime)
   pngToMonoLiner(png).reduce((collection, line) => printer.write(Buffer.from([lineOutput(line)])))
-
-  const job2 = Buffer.from([
+  console.log('Main payload spooled', Date.now() - startTime)
+  const cutJob = Buffer.from([
     ...cut
   ])
-
-  printer.write(job2)
+  console.log('Main payload spooled', Date.now() - startTime)
+  printer.write(cutJob)
+  console.log('Cut', Date.now() - startTime)
   printer.end()
+  console.log('End', Date.now() - startTime)
   return true
 }
