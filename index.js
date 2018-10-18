c17b39ce70fc561f3a62b0bbb8cc21e505a46ce1
Will Stewart <willstewart.decider@gmail.com>
2018-10-18 09:48:18 +0100

    > reduce printing speed

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const {
   lineOutput,
   setup,
   feedAndCut,
-  feed,
-  cut,
   primaryBurnSpeed,
   secondaryBurnSpeed
 } = require('./lib/util')
@@ -16,35 +14,39 @@ const PNG = require('pngjs').PNG
 
 module.exports = function(fileData) {
 
-  console.log('Printer reached \n\n\n\n\n\n***********************\n\n\n\n\n\n')
+  console.log('**********Printer reached *************\n**************************************')
   const startTime = Date.now()
   const png = PNG.sync.read(fileData)
+  let lines = []
   console.log('PNG Read', Date.now() - startTime)
 
-  const job = Buffer.from([
-    ...primaryBurnSpeed(500),
-    ...secondaryBurnSpeed(120),
-    ...setup,
-    ...feed
-  ])
   console.log('Buffer Created', Date.now() - startTime)
   const printer = fs.createWriteStream('/dev/usb/lp0')
   console.log('Connected to printer', Date.now() - startTime)
-  printer.write(job)
   console.log('Write Job Header', Date.now() - startTime)
 
 
   const onLine = line => {
-    printer.write(Buffer.from([lineOutput(line)]))
+    lines.push(line)
   }
 
   pngToMonoLiner(png, onLine, function () {
     console.log('Main payload spooled', Date.now() - startTime)
-    const cutJob = Buffer.from([
-      ...cut
+
+
+    const job = Buffer.from([
+      ...primaryBurnSpeed(500),
+      ...secondaryBurnSpeed(120),
+      ...setup,
+      ...lines.reduce(
+        (collection, line) => collection.concat(...lineOutput(line)),
+        []
+      ),
+      ...feedAndCut
     ])
+
     console.log('Main payload spooled', Date.now() - startTime)
-    printer.write(cutJob)
+    printer.write(job)
     console.log('Cut', Date.now() - startTime)
     printer.end()
     console.log('End', Date.now() - startTime)
