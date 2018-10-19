2e8a1be9f5a43dda50f4983ef8a39db76149be52
Will Stewart <willstewart.decider@gmail.com>
2018-10-19 12:50:02 +0100

    > Clean up logs

--- index.js
+++ index.js
@@ -13,42 +13,41 @@ const pngToMonoLiner = require('./lib/png-mono-liner')
 const UPNG = require('upng-js')
 
 module.exports = function(png) {
-  console.log(
-    '**********Printer reached *************\n**************************************'
-  )
+  console.log('********** Print Job Initiated *************')
   const startTime = Date.now()
 
-  const pngRGB = UPNG.decode(png.data)
   let lines = []
-  console.log('PNG Read', Date.now() - startTime)
 
-  console.log('Buffer Created', Date.now() - startTime)
+  const pngRGB = UPNG.decode(png)
+  console.log('UPNG Read', Date.now() - startTime)
+
   const printer = fs.createWriteStream('/dev/usb/lp0')
-  console.log('Connected to printer', Date.now() - startTime)
-  console.log('Write Job Header', Date.now() - startTime)
+  console.log('USB Write Stream Opened', Date.now() - startTime)
 
   const onLine = line => {
     lines.push(line)
   }
 
   pngToMonoLiner(pngRGB, onLine, function() {
-    console.log('Main payload spooled', Date.now() - startTime)
+    console.log('pngToMonoLiner Completed ', Date.now() - startTime)
 
     const transformedLines = lines.map(line => lineOutput(line))
 
     const mainBody = [].concat.apply([], transformedLines)
+    console.log('Main Body Transformed', Date.now() - startTime)
 
     const primary = primaryBurnSpeed(500)
+    console.log('Primary Burn Speed Generated', Date.now() - startTime)
 
     const secondary = secondaryBurnSpeed(120)
+    console.log('Secondary Burn Speed Generated', Date.now() - startTime)
 
     const job = Buffer.from(
       primary.concat(secondary, setup, mainBody, feedAndCut)
     )
-
-    console.log('Main payload spooled', Date.now() - startTime)
+    console.log('Entire payload spooled', Date.now() - startTime)
     printer.write(job)
-    console.log('Cut', Date.now() - startTime)
+    console.log('Job Written to Printer', Date.now() - startTime)
     printer.end()
     console.log('End', Date.now() - startTime)
     return true
